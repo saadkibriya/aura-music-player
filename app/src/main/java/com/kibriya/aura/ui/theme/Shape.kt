fc4b5310ae44a4cd200a4e45487937c4ffdcb627
Md Golam Kibriya <58004503+saadkibriya@users.noreply.github.com>
2026-05-01 14:21:00 +0600

    Create app/src/main/java/com/kibriya/aura/ui/theme/Shape.kt

---
+++ app/src/main/java/com/kibriya/aura/ui/theme/Shape.kt
@@ -0,0 +1,15 @@
+// MIT License
+// Copyright (c) 2025 Md Golam Kibriya
+package com.kibriya.aura.ui.theme
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Shapes
+import androidx.compose.ui.unit.dp
+
+val AuraShapes = Shapes(
+    extraSmall = RoundedCornerShape(4.dp),
+    small      = RoundedCornerShape(8.dp),
+    medium     = RoundedCornerShape(16.dp),
+    large      = RoundedCornerShape(24.dp),
+    extraLarge = RoundedCornerShape(32.dp)
+)
