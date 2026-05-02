8851e960d62a1a1ece54dfab8ef4440e034d47bd
Md Golam Kibriya <58004503+saadkibriya@users.noreply.github.com>
2026-05-02 11:01:11 +0600

    Update NowPlayingViewModel.kt

--- app/src/main/java/com/kibriya/aura/ui/nowplaying/NowPlayingViewModel.kt
+++ app/src/main/java/com/kibriya/aura/ui/nowplaying/NowPlayingViewModel.kt
@@ -1,25 +1,5 @@
 // MIT License
-//
-// Copyright (c) 2024 Saad Kibriya
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
-
+// Copyright (c) 2025 Md Golam Kibriya
 package com.kibriya.aura.ui.nowplaying
 
 import android.graphics.Bitmap
@@ -51,13 +31,18 @@ class NowPlayingViewModel @Inject constructor(
 
     init {
         viewModelScope.launch {
-            playerRepository.playerState().collect { state ->
+            playerRepository.playerState.collect { state ->
                 _playerState.value = state
             }
         }
     }
 
     fun play(song: Song) {
+        viewModelScope.launch { playerRepository.play(song) }
+    }
+
+    fun play() {
+        val song = _playerState.value.currentSong ?: return
         viewModelScope.launch { playerRepository.play(song) }
     }
 
@@ -86,10 +71,11 @@ class NowPlayingViewModel @Inject constructor(
     }
 
     fun cycleRepeat() {
-        viewModelScope.launch { playerRepository.cycleRepeat() }
+        viewModelScope.launch { playerRepository.cycleRepeatMode() }
     }
 
-    fun toggleFavorite(songId: Long) {
+    fun toggleFavorite() {
+        val songId = _playerState.value.currentSong?.id ?: return
         viewModelScope.launch { songRepository.toggleFavorite(songId) }
     }
 
