15a3cbdfb2156d6b8658acbd434c6bb6a369c07a
Md Golam Kibriya <58004503+saadkibriya@users.noreply.github.com>
2026-05-02 11:31:04 +0600

    Update Color.kt

--- app/src/main/java/com/kibriya/aura/ui/theme/Color.kt
+++ app/src/main/java/com/kibriya/aura/ui/theme/Color.kt
@@ -4,12 +4,13 @@ package com.kibriya.aura.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val AuraBackground   = Color(0xFF0A0A0F)
-val AuraSurface      = Color(0xFF12121A)
-val AuraCard         = Color(0xFF1C1C28)
-val AuraViolet       = Color(0xFF8B5CF6)
-val AuraAmber        = Color(0xFFF59E0B)
-val AuraVioletGlow   = Color(0x668B5CF6)
-val AuraAmberGlow    = Color(0x66F59E0B)
-val AuraOnPrimary    = Color(0xFFF2F2F2)
-val AuraOnSecondary  = Color(0x99FFFFFF)
+val AuraBackground  = Color(0xFF0A0A0F)
+val AuraSurface     = Color(0xFF12121A)
+val AuraCard        = Color(0xFF1C1C28)
+val AuraViolet      = Color(0xFF8B5CF6)
+val AuraAmber       = Color(0xFFF59E0B)
+val AuraVioletGlow  = Color(0x668B5CF6)
+val AuraAmberGlow   = Color(0x66F59E0B)
+val AuraOnPrimary   = Color(0xFFF2F2F2)
+val AuraOnSecondary = Color(0x99FFFFFF)
+val TextSecondary   = Color(0x99FFFFFF)
